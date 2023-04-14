36edf0b01e3bbcb5d488685a0ef695a705aec0c9
Si Jobling <simon.jobling@gmail.com>
2023-04-14 21:40:14 +0100

    Update AppConfig.ts with name and title

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
+  site_name: 'PETALS',
+  title: 'PETALS team health framework',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
