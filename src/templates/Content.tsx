cd100f2120764d2cf002910f2797c574fa89ed89
Si Jobling <si@jobling.com>
2023-05-24 21:04:24 +0100

    Centre and resize heading on About content

--- src/templates/Content.tsx
+++ src/templates/Content.tsx
@@ -4,11 +4,14 @@ import { Section } from '../layout/Section';
 const Content = () => (
   <Background color="bg-amber-300">
     <Section>
-      <h2 className="text-2xl">What is PETALS?</h2>
-      <p>
-        PETALS is a framework to help improve team health, rapidly and
-        regularly, with a tried and tested method from large tech organisations.
-      </p>
+      <div className="text-center">
+        <h2 className="text-4xl text-gray-900 font-bold">What is PETALS?</h2>
+        <p>
+          PETALS is a framework to help improve team health, rapidly and
+          regularly, with a tried and tested method from large tech
+          organisations.
+        </p>
+      </div>
     </Section>
   </Background>
 );
