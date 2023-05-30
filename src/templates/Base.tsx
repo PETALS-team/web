6ea60b0d69e0740cc9169d4c95335f7c3fba2d68
Si Jobling <si@jobling.com>
2023-05-30 20:57:07 +0100

    Package lock updates

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -7,7 +7,7 @@ import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
-  <div className="antialiased text-gray-600">
+  <div className="antialiased text-gray-600 font-poppins">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
     <Content />
