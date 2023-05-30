4cf7dce87a34f508712f535db47c756f7c67c074
Si Jobling <si@jobling.com>
2023-05-30 22:17:25 +0100

    Set app title

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'PETALS',
-  title: 'PETALS team health framework',
+  title: 'PETALS - a simple framework to improve team health',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
