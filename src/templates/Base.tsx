a2bd6a9c45e3e3c0b4ef5b4ec80bb5c72c3ca38c
Si Jobling <si@jobling.com>
2023-06-06 19:58:32 +0100

    Set base font colour to darker grey

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -7,7 +7,7 @@ import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
-  <div className="antialiased text-gray-600 font-gaegu">
+  <div className="antialiased text-gray-800 font-gaegu">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
     <Content />
