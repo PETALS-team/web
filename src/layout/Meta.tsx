e0d7a62c110ca04bed9439a61a58245600b8c8a2
Si Jobling <simon.jobling@gmail.com>
2023-06-30 22:31:50 +0100

    start configuring OG image

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -67,6 +67,20 @@ const Meta = (props: IMetaProps) => {
           url: props.canonical,
           locale: AppConfig.locale,
           site_name: AppConfig.site_name,
+          images: [
+            {
+              url: 'https://www.example.ie/og-image.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+            },
+            {
+              url: 'https://www.example.ie/og-image-2.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt 2',
+            },
+          ],
         }}
       />
     </>
