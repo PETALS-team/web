cec4650fa333770d6d042139e5c37a7598f1b232
Si Jobling <si@jobling.com>
2023-07-03 13:23:10 +0000

    Tidy up body font size and spacing

--- src/templates/Content.tsx
+++ src/templates/Content.tsx
@@ -8,20 +8,20 @@ const Content = () => (
         <h2 className="text-4xl text-gray-900 font-gaegu font-bold pb-10">
           Why use PETALS?
         </h2>
-        <p>
+        <p className="leading-9">
           Do you find your team&apos;s morale peaks and troughs without any
           direction?
         </p>
-        <p>
+        <p className="leading-9">
           Have you observed teams you lead struggle to identify improvement
           areas?
         </p>
-        <p>
+        <p className="leading-9">
           Are teams you&apos;re working with suggesting they&apos;re awesome but
           can&apos;t pinpoint why?
         </p>
-        <p className="pt-20">
-          <strong>
+        <p className="pt-20 leading-9">
+          <strong className="text-lg">
             PETALS is a framework to help improve team health, rapidly and
             regularly, with a tried and tested method from large tech
             organisations.
--- src/templates/Origins.tsx
+++ src/templates/Origins.tsx
@@ -4,17 +4,17 @@ import { Section } from '../layout/Section';
 const Origins = () => (
   <Background color="bg-amber-300">
     <Section id="Origins">
-      <div className="text-center text-xl">
+      <div className="text-center">
         <h2 className="text-4xl text-gray-900 font-gaegu font-bold pb-10">
           Where did PETALS come from?
         </h2>
-        <p>
+        <p className="leading-9">
           Si Jobling is an experienced Engineering Manager, former Agile
           Delivery Manager and Web Engineer. He has worked across a variety of
           organisations at different scales and context, from the UK to USA and
           China, always driving for healthy open cultures in tech.
         </p>
-        <p className="mt-5">
+        <p className="leading-9 mt-5">
           With over 20 years experience of hands on software engineering and
           leadership, he knows what makes for healthy engineering teams and
           software engineers. He originally designed PETALS as a personal
