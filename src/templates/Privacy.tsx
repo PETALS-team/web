09d132214bdfef1dd159bb66c3fa526430853fb9
Si Jobling <si@jobling.com>
2023-07-19 16:12:11 +0100

    Privacy page content

---
+++ src/templates/Privacy.tsx
@@ -0,0 +1,156 @@
+import { Background } from '../background/Background';
+import { Section } from '../layout/Section';
+
+const Privacy = () => (
+  <Background color="bg-amber-300">
+    <Section id="Privacy">
+      <div className="">
+        <h1 className="text-4xl font-gaegu">Privacy Policy</h1>
+        <p>Effective Date: 5 July 2023</p>
+        <p>
+          This Privacy Policy describes how we collect, use, store, and disclose
+          the information we obtain from users (&quot;you&quot; or
+          &quot;user&quot;) when you visit our website and provide your email
+          address for newsletter subscription (&quot;Service&quot;). Please read
+          this Privacy Policy carefully to understand our practices regarding
+          your personal information and how we will treat it.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy1">
+          1. Information We Collect
+        </h2>
+        <h3 className="text-xl font-gaegu" id="Policy1_1">
+          1.1 Personal Information:
+        </h3>
+        <p>
+          When you subscribe to our newsletter, we collect your email address.
+          We may also collect other personally identifiable information that you
+          voluntarily provide, such as your name, location, or any additional
+          information you choose to share.
+        </p>
+        <h3 className="text-xl font-gaegu" id="Policy1_2">
+          1.2 Non-Personal Information:
+        </h3>
+        <p>
+          We may also collect non-personal information automatically as you
+          navigate and interact with our website. This information may include
+          your IP address, browser type, device information, referring/exit
+          pages, and operating system. This data is collected through various
+          technologies such as cookies, web beacons, and similar tracking
+          technologies.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy2">
+          2. Use of Information
+        </h2>
+        <h3 className="text-xl font-gaegu" id="Policy2_1">
+          2.1 Personal Information:
+        </h3>
+        <p>
+          We use the personal information we collect to send you newsletters,
+          updates, promotions, and other relevant communications based on your
+          subscription preferences. We may also use your email address to
+          respond to your inquiries, provide customer support, and improve our
+          services.
+        </p>
+        <h3 className="text-xl font-gaegu" id="Policy2_2">
+          2.2 Non-Personal Information:
+        </h3>
+        <p>
+          The non-personal information we collect is used to analyze trends,
+          administer the website, track users&apos; movements around the
+          website, and gather demographic information for aggregate use. This
+          data helps us to understand how users interact with our website and
+          improve its functionality.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy3">
+          3. Data Retention
+        </h2>
+        <p>
+          We will retain your personal information for as long as necessary to
+          fulfill the purposes outlined in this Privacy Policy, unless a longer
+          retention period is required or permitted by law. If you wish to
+          unsubscribe from our newsletter or request the deletion of your
+          personal information, please refer to the &quot;
+          <a href="#Policy6">Your Rights and Choices</a>&quot; section below.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy4">
+          4. Information Sharing
+        </h2>
+        <p>
+          We may share your personal information with trusted third-party
+          service providers who assist us in operating our website and
+          delivering the Service. These third-party service providers have
+          access to your personal information only to perform specific tasks on
+          our behalf and are obligated not to disclose or use it for any other
+          purpose.
+        </p>
+        <p>
+          Additionally, we may disclose your information if required by law, to
+          protect our rights or the safety of others, or as part of a merger,
+          acquisition, or sale of assets.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy5">
+          5. Data Security
+        </h2>
+        <p>
+          We implement reasonable security measures to protect your personal
+          information from unauthorized access, alteration, disclosure, or
+          destruction. However, no method of transmission over the internet or
+          electronic storage is 100% secure, and we cannot guarantee absolute
+          security of your data.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy6">
+          6. Your Rights and Choices
+        </h2>
+        <p>
+          You have the right to access, update, correct, and delete your
+          personal information held by us. You may also unsubscribe from our
+          newsletter at any time by following the instructions provided in the
+          email. If you have any questions or requests regarding your personal
+          information, please{' '}
+          <a href="#Policy9">contact us using the information provided below</a>
+          .
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy7">
+          7. Children&apos;s Privacy
+        </h2>
+        <p>
+          Our website and Service are not directed toward individuals under the
+          age of 13. We do not knowingly collect personal information from
+          children. If you are a parent or guardian and believe that your child
+          has provided us with personal information without your consent, please
+          <a href="#Policy9"> contact us</a> immediately so that we can delete
+          such information.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy8">
+          8. Changes to the Privacy Policy
+        </h2>
+        <p>
+          We reserve the right to modify or update this Privacy Policy at any
+          time. The effective date at the beginning of this policy indicates the
+          date of the most recent update. We encourage you to review this
+          Privacy Policy periodically for any changes. Your continued use of our
+          website after any modifications to this Privacy Policy constitutes
+          your acceptance of such changes.
+        </p>
+        <h2 className="text-2xl font-gaegu" id="Policy9">
+          9. Contact Us
+        </h2>
+        <p>
+          If you have any questions, concerns, or requests regarding this
+          Privacy Policy or our practices, please contact us at:
+        </p>
+        <p>
+          Unstyled, Corner House, Laud&apos;s Road, Northampton, NN6 7TJ
+          <a href="mailto:webmaster@petals.team"> webmaster@petals.team</a>
+        </p>
+        <p>
+          By using our website and providing your email address for newsletter
+          subscription, you acknowledge that you have read and understood this
+          Privacy Policy and agree to be bound by its terms and conditions.
+        </p>
+      </div>
+    </Section>
+  </Background>
+);
+
+export { Privacy };
