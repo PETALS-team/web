99ebc9202bc528a2d96a3fb224c6d9a2ba5f8b31
Si Jobling <si@jobling.com>
2023-09-11 09:22:27 +0000

    Create Snapshots page

---
+++ src/pages/snapshots.tsx
@@ -0,0 +1,11 @@
+import { Base } from '../templates/Base';
+
+const SnapshotsPage = () => (
+  <Base>
+    <div>
+      <h1>Snapshots</h1>
+    </div>
+  </Base>
+);
+
+export default SnapshotsPage;
