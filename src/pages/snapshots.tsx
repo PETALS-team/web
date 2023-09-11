5cdd72bd38998e74a30e09bb481cac9fdb6a80da
Si Jobling <si@jobling.com>
2023-09-11 11:18:09 +0000

    Add Youtube and Spotify embeds to Snapshots page

--- src/pages/snapshots.tsx
+++ src/pages/snapshots.tsx
@@ -1,10 +1,46 @@
+import { Background } from '../background/Background';
+import { Section } from '../layout/Section';
 import { Base } from '../templates/Base';
 
 const SnapshotsPage = () => (
   <Base>
-    <div>
-      <h1>Snapshots</h1>
-    </div>
+    <Background color="bg-amber-300">
+      <Section id="Snapshots">
+        <h1 className="text-4xl font-gaegu">PETALS Snapshots</h1>
+        <p>
+          Follow the journey of how PETALS is growing with regular reflections
+          and insights from the creator, Si.
+        </p>
+        <div className=" items-left space-x-4 py-6">
+          <div id="Youtube">
+            <h2 className="text-3xl font-gaegu">Watch on Youtube</h2>
+            <iframe
+              width="100%"
+              height="315"
+              src="https://www.youtube-nocookie.com/embed/videoseries?si=X6cBcTDIFwiDWdy2&amp;list=PLKbU77Ew5WTEGzy0nUAvnDPlKWcyRId9X"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              className="rounded-xl"
+            ></iframe>
+          </div>
+          <div id="Spotify">
+            <h2 className="text-3xl font-gaegu">Listen on Spotify</h2>
+            <iframe
+              className="rounded-xl"
+              src="https://open.spotify.com/embed/show/1dGwBZFMtlLtcJG4nbQYGS?utm_source=generator&theme=0"
+              width="100%"
+              height="152"
+              frameBorder="0"
+              allowFullScreen
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+      </Section>
+    </Background>
   </Base>
 );
 
