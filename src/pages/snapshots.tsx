99ead39dcf9f5730c9b04de42d05845169a471e8
Si Jobling <si@jobling.com>
2023-09-12 21:42:28 +0100

    Links to Snapshots feeds on page

--- src/pages/snapshots.tsx
+++ src/pages/snapshots.tsx
@@ -25,33 +25,58 @@ const SnapshotsPage = () => (
           format. Choose your favourite and provide feedback through the local
           comment threads.
         </p>
-        <div className="py-6">
-          <div id="Youtube">
-            <h2 className="text-3xl font-gaegu my-6">Watch on Youtube</h2>
-            <iframe
-              width="100%"
-              height="315"
-              src="https://www.youtube-nocookie.com/embed/videoseries?si=X6cBcTDIFwiDWdy2&amp;list=PLKbU77Ew5WTEGzy0nUAvnDPlKWcyRId9X"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-              className="rounded-xl"
-            ></iframe>
-          </div>
-          <div id="Spotify">
-            <h2 className="text-3xl font-gaegu my-6">Listen on Spotify</h2>
-            <iframe
-              className="rounded-xl"
-              src="https://open.spotify.com/embed/show/1dGwBZFMtlLtcJG4nbQYGS?utm_source=generator&theme=0"
-              width="100%"
-              height="152"
-              frameBorder="0"
-              allowFullScreen
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-            ></iframe>
-          </div>
+        <div id="Youtube">
+          <h2 className="text-3xl font-gaegu my-6">Watch on Youtube</h2>
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube-nocookie.com/embed/videoseries?si=X6cBcTDIFwiDWdy2&amp;list=PLKbU77Ew5WTEGzy0nUAvnDPlKWcyRId9X"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            className="rounded-xl"
+          ></iframe>
+        </div>
+        <div id="Spotify">
+          <h2 className="text-3xl font-gaegu my-6">Listen on Spotify</h2>
+          <iframe
+            className="rounded-xl"
+            src="https://open.spotify.com/embed/show/1dGwBZFMtlLtcJG4nbQYGS?utm_source=generator&theme=0"
+            width="100%"
+            height="152"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+        </div>
+        <h2 className="text-3xl font-gaegu my-6">Follow along</h2>
+        <div className="flex align-middle">
+          <a
+            href="https://youtube.com/playlist?list=PLKbU77Ew5WTEGzy0nUAvnDPlKWcyRId9X"
+            className="flex-initial bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://open.spotify.com/show/1dGwBZFMtlLtcJG4nbQYGS?si=t5Mq8zbXRt-cD9QSo9zWow"
+            className="flex-initial bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-2 rounded"
+          >
+            Spotify
+          </a>
+          <a
+            href="https://podcasts.apple.com/us/podcast/petals/id1698197915"
+            className="flex-initial bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 mx-2 rounded"
+          >
+            Apple Podcasts
+          </a>
+          <a
+            href="https://petals.team/podcast"
+            className="flex-initial bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 mx-2 rounded"
+          >
+            Podcast RSS
+          </a>
         </div>
       </Section>
     </Background>
