7041b9dd03c802c12f6d049658c94d6940da3c26
Si Jobling <si@jobling.com>
2023-11-12 20:05:44 +0000

    Move Google Fonts to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,16 @@ function CustomApp({ Component, pageProps }: AppProps) {
                     });
                 `}
       </Script>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&family=Gaegu:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
 
       <Head>
         <title>Welcome!</title>
