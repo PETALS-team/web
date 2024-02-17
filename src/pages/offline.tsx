5b1561d543463115694c445547491d9772f3edf5
Si Jobling <si@jobling.com>
2024-02-17 16:05:06 +0000

    Fix linting rules for Offline page

--- src/pages/offline.tsx
+++ src/pages/offline.tsx
@@ -8,10 +8,11 @@ const SnapshotsPage = () => (
     <Background color="bg-amber-300">
       <Section id="Toolkits">
         <h1 className="text-4xl font-gaegu text-center mb-6">
-          PETALS Offline IRL 
+          PETALS Offline IRL
         </h1>
         <p className="my-12">
-          Fancy taking your PETALS conversations offline to a meeting room or even pub? Here's some tips and suggestions.
+          Fancy taking your PETALS conversations offline to a meeting room or
+          even pub? Here&apos;s some tips and suggestions.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div id="GetData" className="relative inset-0 ">
@@ -24,7 +25,8 @@ const SnapshotsPage = () => (
               Get team data
             </h2>
             <p className="my-6">
-              Use which toolkit you prefer to get everyone's scores along with the analysis 
+              Use which toolkit you prefer to get everyone&apos;s scores along
+              with the analysis
             </p>
           </div>
           <div id="GetReady" className="relative inset-0 ">
@@ -33,24 +35,38 @@ const SnapshotsPage = () => (
               alt="Google Workspace"
               className=" w-10 m-auto"
             />
-            <h2 className="text-3xl font-gaegu text-center mb-6">
-              Get ready
-            </h2>
+            <h2 className="text-3xl font-gaegu text-center mb-6">Get ready</h2>
             <p className="my-6">
               Print out the resulting analysis or take a screen to share.
             </p>
             <p className="my-6">
-              Grab some sticky notes and pens, here's some recommendations:
-              
-              <a href="https://amzn.to/49ERv4S">Multi-coloured bubble shape sticky notes</a> or 
-              <a href="https://amzn.to/3I4rmQU">multi-coloured square sticky notes</a>
+              Grab some sticky notes and pens, here&apos;s some recommendations:
+              <a href="https://amzn.to/49ERv4S">
+                Multi-coloured bubble shape sticky notes
+              </a>
+              or
+              <a href="https://amzn.to/3I4rmQU">
+                multi-coloured square sticky notes
+              </a>
             </p>
-            <p className="my-6">Book a space big enough for people to sit comfortably but also move about, ideally with a large wall or table to place your thoughts</p>
-            <p className="my-6">Spread out the five headings across the space in the nominated colours:</p>
             <p className="my-6">
-            <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">Teamwork</span>
+              Book a space big enough for people to sit comfortably but also
+              move about, ideally with a large wall or table to place your
+              thoughts
             </p>
-            <p>Leave space above and below each heading for positives (above) and negatives (below)</p>
+            <p className="my-6">
+              Spread out the five headings across the space in the nominated
+              colours:
+            </p>
+            <p className="my-6">
+              <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+                Teamwork
+              </span>
+            </p>
+            <p>
+              Leave space above and below each heading for positives (above) and
+              negatives (below)
+            </p>
           </div>
           <div id="GetTalking" className="relative inset-0 ">
             <img
@@ -62,17 +78,37 @@ const SnapshotsPage = () => (
               Get talking
             </h2>
             <p className="my-6">
-              Start the session by reviewing the results from the data collection
+              Start the session by reviewing the results from the data
+              collection
             </p>
             <p className="my-6">
-              Encourage people to share their personal scores along with why 
+              Encourage people to share their personal scores along with why
             </p>
-            <p className="my-6">Spend 5 minutes writing down thoughts about wins and challenges for each topic</p>
-            <p className="my-6">Ask each team member to take turns placing their notes around the headings, positives above the heading and negatives below</p>
-            <p className="my-6">Group notes together as you go around the team's discussions, elaborating if unclear but timeboxing to 3 minutes</p>
-            <p className="my-6">Ask every participant to dot-vote their most important discussions, 2-5 each, to determine the most important topics</p>
-            <p className="my-6">Collectively discuss and agree the immmediate actions to focus on, consider arranging dedicated conversations if required (either in person or via text chats)</p>
-            <p className="my-6">Track the actions somewhere clear for everyone to remember and follow up on, maybe the team's project board?</p>
+            <p className="my-6">
+              Spend 5 minutes writing down thoughts about wins and challenges
+              for each topic
+            </p>
+            <p className="my-6">
+              Ask each team member to take turns placing their notes around the
+              headings, positives above the heading and negatives below
+            </p>
+            <p className="my-6">
+              Group notes together as you go around the team&apos;s discussions,
+              elaborating if unclear but timeboxing to 3 minutes
+            </p>
+            <p className="my-6">
+              Ask every participant to dot-vote their most important
+              discussions, 2-5 each, to determine the most important topics
+            </p>
+            <p className="my-6">
+              Collectively discuss and agree the immmediate actions to focus on,
+              consider arranging dedicated conversations if required (either in
+              person or via text chats)
+            </p>
+            <p className="my-6">
+              Track the actions somewhere clear for everyone to remember and
+              follow up on, maybe the team&apos;s project board?
+            </p>
           </div>
         </div>
         <Banner />
