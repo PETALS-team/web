029a6016165bd77e13f8e2dc05cc3cb345fd60ae
Si Jobling <si@jobling.com>
2024-02-28 21:42:19 +0000

    Tidy up app page content and formatting

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -10,38 +10,63 @@ const AppPage = () => (
         <h1 className="text-4xl font-gaegu text-center mb-6">
           Get early access to the PETALS App
         </h1>
-        <p className="my-12">
-          Join our waiting list for exclusive early access to the bespoke
+        <p className="my-5">
+          Join the waiting list for exclusive early access to the bespoke
           PETALS, built to streamline your own PETALS discussions around your
-          own schedule
+          own schedule.
+        </p>
+        <p className="my-5">Our invite only access provides:</p>
+        <ul>
+          <li>Organisation configuration</li>
+          <li>Multiple team member onboarding</li>
+          <li>Metrics reports for each team</li>
+          <li>Bespoke account management &amp; regular support</li>
+        </ul>
+        <p className="my-8">
+          Simply provide some basic details and we&apos;ll invite you personally
+          when the app is ready.
         </p>
         <form
           action="https://buttondown.email/api/emails/embed-subscribe/petalsteam"
           method="post"
           target="popupwindow"
-          className="embeddable-buttondown-form"
+          className="embeddable-buttondown-form grid grid-cols-1 md:grid-cols-4 gap-6"
         >
-          <label htmlFor="bd-email">Your email address</label>
-          <input type="email" name="email" id="bd-email" />
-          <label htmlFor="bd-organisaton">Your organisation name</label>
-          <input
-            type="text"
-            name="metadata__organisation"
-            id="bd-organisation"
-          />
-          <label htmlFor="bd-first-name">Your first name</label>
-          <input type="text" name="metadata__first_name" id="bd-first-name" />
-          <input type="submit" value="Subscribe" />
-          <input type="hidden" name="tag" value="App" />
-          <p>
-            <a
-              href="https://buttondown.email/refer/petalsteam"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Powered by Buttondown.
-            </a>
-          </p>
+          <div className="relative inset-0 ">
+            <label htmlFor="bd-email">Your email address</label>
+            <input
+              type="email"
+              name="email"
+              id="bd-email"
+              className="block w-full rounded-md border-1 p-2 "
+            />
+          </div>
+          <div className="relative inset-0 ">
+            <label htmlFor="bd-first-name">Your first name</label>
+            <input
+              type="text"
+              name="metadata__first_name"
+              id="bd-first-name"
+              className="block w-full rounded-md border-1 p-2 "
+            />
+          </div>
+          <div className="relative inset-0 ">
+            <label htmlFor="bd-organisaton">Your organisation name</label>
+            <input
+              type="text"
+              name="metadata__organisation"
+              id="bd-organisaton"
+              className="block w-full rounded-md border-1 p-2 "
+            />
+          </div>
+          <div className="relative inset-0 ">
+            <input
+              type="submit"
+              value="Sign up"
+              className="rounded-md bg-slate-600 px-10 py-5 text-sm font-semibold text-white "
+            />
+            <input type="hidden" name="tag" value="App" />
+          </div>
         </form>
         <Banner />
       </Section>
