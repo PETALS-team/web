5417c5216e2d7946f04fcdaa5457da45132c2ae9
Si Jobling <si@jobling.com>
2024-03-12 19:59:53 +0000

    Remove crossOrigin from Gfont link

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -51,11 +51,7 @@ const Meta = (props: IMetaProps) => {
           key="favicon"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&family=Gaegu:wght@400;700&display=swap"
           rel="stylesheet"
