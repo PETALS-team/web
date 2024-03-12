ae3ceb2e23f60519725cf82c10ffa3481bf53f0d
Si Jobling <si@jobling.com>
2024-03-12 19:58:51 +0000

    Add Fathom tracking for Next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import * as Fathom from 'fathom-client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Script from 'next/script';
 
 import '../styles/global.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load('PHFUPZYT', {
+      includedDomains: ['petals.team'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
   return (
     <>
       <Script
