0b58d47cb2b413091e80de621aaf3f1174839096
Si Jobling <si.jobling@rightmove.co.uk>
2024-08-06 20:05:42 +0100

    Apply hero form component to CTA banner

--- src/cta/CTABanner.tsx
+++ src/cta/CTABanner.tsx
@@ -7,14 +7,13 @@ type ICTABannerProps = {
 };
 
 const CTABanner = (props: ICTABannerProps) => (
-  <div className="text-center flex flex-col p-4 sm:text-left sm:flex-row sm:items-center sm:justify-between sm:p-12 bg-primary-100 rounded-md">
-    <div className="font-semibold">
-      <div className="text-gray-900 text-2xl font-gaegu">{props.title}</div>
-      <div className="text-gray-600 text-base mt-6">{props.subtitle}</div>
-    </div>
-
-    <div className="whitespace-no-wrap mt-3 sm:mt-0 sm:ml-10">
-      {props.button}
+  <div className="hero bg-base-200 min-h-screen">
+    <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="text-center lg:text-left">
+        <div className="text-2xl font-bold">{props.title}</div>
+        <div className="py-6">{props.subtitle}</div>
+      </div>
+      <div className="">{props.button}</div>
     </div>
   </div>
 );
