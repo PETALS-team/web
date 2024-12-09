a1e07212075b1d02dec499021fe25deeaadaa7fa
Si Jobling <si@jobling.com>
2024-12-09 12:51:10 +0000

    Tidy up Brevo inline form height on banner

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 import { CTABanner } from '../cta/CTABanner';
 import { Section } from '../layout/Section';
 
@@ -9,7 +7,11 @@ const Banner = () => (
       title="Get your free toolkit to start improving your team health"
       subtitle="Join the PETALS newsletter with hundreds other users"
       button={
-        <iframe width="335" height="155" src="https://275970ae.sibforms.com/serve/MUIFAO5bUiWjOyQAArfA7zFnzFkYIZ3WRhZtVWdR0XdFKarGcDnfOvHXWG_yFMTs7V2vVdA4n3WgO1Tm5Y9ZnOpusweI2vY9DxhlCDPUiw5OrPzltbnSBqeeE0iQbTEP4Nq3axt9RFVEfN4wCTizPGYsM7owfd9cQkz1ds8P4IBwJ5oQFmhlOMHSypsr8qxETBl9UU7qo0H-Bokn"></iframe>
+        <iframe
+          width="335"
+          height="310"
+          src="https://275970ae.sibforms.com/serve/MUIFAO5bUiWjOyQAArfA7zFnzFkYIZ3WRhZtVWdR0XdFKarGcDnfOvHXWG_yFMTs7V2vVdA4n3WgO1Tm5Y9ZnOpusweI2vY9DxhlCDPUiw5OrPzltbnSBqeeE0iQbTEP4Nq3axt9RFVEfN4wCTizPGYsM7owfd9cQkz1ds8P4IBwJ5oQFmhlOMHSypsr8qxETBl9UU7qo0H-Bokn"
+        ></iframe>
       }
     />
   </Section>
